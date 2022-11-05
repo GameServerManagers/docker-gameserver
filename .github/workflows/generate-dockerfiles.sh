f1a534c83542c8e5a0cb38998bdb24b4f0f99fe4
Daniel Gibbs <me@danielgibbs.co.uk>
2022-11-05 23:57:15 +0000

    generate dockerfiles

---
+++ .github/workflows/generate-dockerfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/lgsm/data/serverlist.csv"
+csvjson serverlist.csv >serverlist.json
+
+while read line; do
+  export shortname=$(echo "$line" | awk -F, '{ print $1 }')
+  export servername=$(echo "$line" | awk -F, '{ print $2 }')
+  export gamename=$(echo "$line" | awk -F, '{ print $3 }')
+  export distro="ubuntu-22.04"
+  touch "../../dockerfiles/Dockerfile.${shortname}"
+  echo "Generating ${gamename}: Dockerfile.${shortname}"
+  j2 -f env ../../Dockerfile.j2 >"../../dockerfiles/Dockerfile.${shortname}"
+done <serverlist.csv
+rm serverlist.csv* serverlist.json*
