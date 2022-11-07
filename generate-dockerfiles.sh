5606bc8adf39e822bee18c23991ce1747020935e
Daniel Gibbs <me@danielgibbs.co.uk>
2022-11-07 00:06:40 +0000

    publish multiple dockerfiles

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -2,6 +2,8 @@
 
 wget "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/lgsm/data/serverlist.csv"
 
+echo -n "[" >"shortnamearray.txt"
+
 while read line; do
   export shortname=$(echo "$line" | awk -F, '{ print $1 }')
   export servername=$(echo "$line" | awk -F, '{ print $2 }')
@@ -10,4 +12,7 @@ while read line; do
   touch "dockerfiles/Dockerfile.${shortname}"
   echo "Generating Dockerfile.${shortname} (${gamename})"
   j2 -f env Dockerfile.j2 >"dockerfiles/Dockerfile.${shortname}"
+  echo -n "${shortname}, " >>"shortnamearray.txt"
 done <serverlist.csv
+cat "shortnamearray.txt" | sed 's/.$//' | sed 's/.$//' >"shortname.txt"
+echo -n "]" >>"shortname.txt"
