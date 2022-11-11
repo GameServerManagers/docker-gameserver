19b3e3dfac88da5a5b9f10c5b8ae99eb1204df8f
Daniel Gibbs <me@danielgibbs.co.uk>
2022-11-11 00:41:26 +0000

    test

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -2,8 +2,8 @@
 
 wget "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/lgsm/data/serverlist.csv"
 
-echo "{" >"shortnamearray.json"
-echo "\"include\": [" >>"shortnamearray.json"
+echo -n "{" >"shortnamearray.json"
+echo -n "\\\"include\\\":[" >>"shortnamearray.json"
 
 while read line; do
   export shortname=$(echo "$line" | awk -F, '{ print $1 }')
@@ -13,11 +13,11 @@ while read line; do
   touch "dockerfiles/Dockerfile.${shortname}"
   echo "Generating Dockerfile.${shortname} (${gamename})"
   j2 -f env Dockerfile.j2 >"dockerfiles/Dockerfile.${shortname}"
-  echo "{" >>"shortnamearray.json"
-  echo -n "\"shortname\":" >>"shortnamearray.json"
-  echo "\"${shortname}\"" >>"shortnamearray.json"
-  echo "}," >>"shortnamearray.json"
+  echo -n "{" >>"shortnamearray.json"
+  echo -n "\\\"shortname\\\":" >>"shortnamearray.json"
+  echo -n "\\\"${shortname}\\\"" >>"shortnamearray.json"
+  echo -n "}," >>"shortnamearray.json"
 done <serverlist.csv
 sed -i '$ s/.$//' "shortnamearray.json"
-echo "]" >>"shortnamearray.json"
+echo -n "]" >>"shortnamearray.json"
 echo -n "}" >>"shortnamearray.json"
