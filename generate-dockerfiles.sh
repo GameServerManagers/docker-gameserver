a64ea055a4fa6ec1ea171855f706b02bd4247604
Daniel Gibbs <me@danielgibbs.co.uk>
2023-01-13 21:16:42 +0000

    feat: images are now picked from linuxgsm csv

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -9,7 +9,7 @@ while read line; do
   export shortname=$(echo "$line" | awk -F, '{ print $1 }')
   export servername=$(echo "$line" | awk -F, '{ print $2 }')
   export gamename=$(echo "$line" | awk -F, '{ print $3 }')
-  export distro="ubuntu-22.04"
+  export distro=$(echo "$line" | awk -F, '{ print $4 }')
   touch "dockerfiles/Dockerfile.${shortname}"
   echo "Generating Dockerfile.${shortname} (${gamename})"
   j2 -f env Dockerfile.j2 >"dockerfiles/Dockerfile.${shortname}"
