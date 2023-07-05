3c020b7a02f646562d8503b94eba93080a9e1c16
Daniel Gibbs <me@danielgibbs.co.uk>
2023-07-05 16:00:14 +0100

    feat: generate docker-compose.yml files
    This commit adds a new step to the workflow that generates docker-compose.yml files using the `generate-docker-compose.sh` script. The script downloads a server list, iterates through each entry, and generates a corresponding `docker-compose-${shortname}.yml` file based on the template `docker-compose.yml.j2`. The generated files are then added and committed.

---
+++ generate-docker-compose.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+wget "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/lgsm/data/serverlist.csv"
+
+echo -n "{" >"shortnamearray.json"
+echo -n "\"include\":[" >>"shortnamearray.json"
+
+while read line; do
+  export shortname=$(echo "$line" | awk -F, '{ print $1 }')
+  export servername=$(echo "$line" | awk -F, '{ print $2 }')
+  export gamename=$(echo "$line" | awk -F, '{ print $3 }')
+  export distro=$(echo "$line" | awk -F, '{ print $4 }')
+  touch "docker-compose/docker-compose-${shortname}.yml"
+  echo "Generating docker-compose-${shortname}.yml (${gamename})"
+  j2 -f env docker-compose.yml.j2 >"docker-compose/docker-compose-${shortname}.yml"
+  echo -n "{" >>"shortnamearray.json"
+  echo -n "\"shortname\":" >>"shortnamearray.json"
+  echo -n "\"${shortname}\"" >>"shortnamearray.json"
+  echo -n "}," >>"shortnamearray.json"
+done < <(tail -n +1 serverlist.csv)
+sed -i '$ s/.$//' "shortnamearray.json"
+echo -n "]" >>"shortnamearray.json"
+echo -n "}" >>"shortnamearray.json"
+rm serverlist.csv
