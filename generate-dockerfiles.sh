5af2d63a4a0ad42af8174f11cd7ec5eaa554251e
b13rg <13488329+B13rg@users.noreply.github.com>
2024-09-18 19:15:05 -0700

    adjust how dockerfile vars are fetched and recalled

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-curl -O "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/lgsm/data/serverlist.csv"
+echo "Fetching server list"
+curl -o ./data/serverlist.csv "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/lgsm/data/serverlist.csv"
 
-echo -n "{" >"shortnamearray.json"
-echo -n "\"include\":[" >>"shortnamearray.json"
+echo '{"include": {}}' > ./data/shortnamearray.json
 
+# convert csv to json
 while read line; do
   export shortname=$(echo "$line" | awk -F, '{ print $1 }')
   export servername=$(echo "$line" | awk -F, '{ print $2 }')
   export gamename=$(echo "$line" | awk -F, '{ print $3 }')
   export distro=$(echo "$line" | awk -F, '{ print $4 }')
-  touch "dockerfiles/Dockerfile.${shortname}"
+
+  yq -iP '.include[strenv(shortname)]={"shortname": strenv(shortname),"servername": strenv(servername),"gamename": strenv(gamename),"distro": strenv(distro)}' ./data/shortnamearray.json -o json
+done < <(tail -n +2 ./data/serverlist.csv)
+
+echo "Found $(yq '.include | keys | length' ./data/shortnamearray.json) items"
+
+while read sname; do
+  export gamename=$(yq ".include[strenv(shortname)].gamename" data/shortnamearray.json)
+  export shortname=$sname
   echo "Generating Dockerfile.${shortname} (${gamename})"
-  jinjanate Dockerfile.j2 >"dockerfiles/Dockerfile.${shortname}"
-  echo -n "{" >>"shortnamearray.json"
-  echo -n "\"shortname\":" >>"shortnamearray.json"
-  echo -n "\"${shortname}\"" >>"shortnamearray.json"
-  echo -n "}," >>"shortnamearray.json"
-done < <(tail -n +2 serverlist.csv)
-sed -i '$ s/.$//' "shortnamearray.json"
-echo -n "]" >>"shortnamearray.json"
-echo -n "}" >>"shortnamearray.json"
-rm serverlist.csv
+  yq '.game=.include[env(shortname)]' data/shortnamearray.json | jinjanate --quiet -f json Dockerfile.j2 - > "dockerfiles/Dockerfile.${shortname}"
+done < <(yq -r ".include| keys[]" data/shortnamearray.json )
