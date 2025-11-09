50bb6be2beb084a4ebdb0b5ea5dac9a1d35c188d
Daniel Gibbs <me@danielgibbs.co.uk>
2025-11-09 00:07:30 +0000

    chore: shellcheck feedback
    * Changed `while read line` to `while read -r line` to prevent backslash interpretation.

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -5,7 +5,7 @@ curl -O "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/lg
 echo -n "{" >"shortnamearray.json"
 echo -n "\"include\":[" >>"shortnamearray.json"
 
-while read line; do
+while read -r line; do
   shortname=$(echo "$line" | awk -F, '{ print $1 }')
   export shortname
   servername=$(echo "$line" | awk -F, '{ print $2 }')
